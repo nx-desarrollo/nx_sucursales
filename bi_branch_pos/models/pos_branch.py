3b19f1121f0197d20ca23df1df4fd017af5e4394
nx-edgar-aldana <edgar.aldana@nimetrix.com>
2025-06-30 22:23:13 -0400

    [FIX] Modificación en método que crea asientos de pagos de POS (método sobrescrito para evitar errores en secuencias de dichos asientos)

--- bi_branch_pos/models/pos_branch.py
+++ bi_branch_pos/models/pos_branch.py
@@ -251,18 +251,80 @@ class PosPayment(models.Model):
             rec.branch_id = rec.session_id.branch_id.id if rec.session_id and rec.session_id.branch_id else False
         return res    
 
-    # Asientos contables del pago del punto de venta:
+    # OVERRIDE (para agregar el branch_id al asiento de pago del punto de venta):
     def _create_payment_moves(self, is_reverse=False):
-        res = super(PosPayment, self)._create_payment_moves(is_reverse)
-        # Nota: res son registros de account.move
-        for rec in res:
-            if rec.pos_payment_ids:
-                payment_id = rec.pos_payment_ids[0]
-                if payment_id.branch_id:
-                    rec.write({
-                        'branch_id': payment_id.branch_id.id
-                    })
-        return res
+        result = self.env['account.move']
+        credit_line_ids = []
+        change_payment = self.filtered(lambda p: p.is_change and p.payment_method_id.type == 'cash')
+        payment_to_change = self.filtered(lambda p: not p.is_change and p.payment_method_id.type == 'cash')[:1]
+        for payment in self - change_payment:
+            order = payment.pos_order_id
+            payment_method = payment.payment_method_id
+            if payment_method.type == 'pay_later' or float_is_zero(payment.amount, precision_rounding=order.currency_id.rounding):
+                continue
+            accounting_partner = self.env["res.partner"]._find_accounting_partner(payment.partner_id)
+            pos_session = order.session_id
+            journal = pos_session.config_id.journal_id
+            if change_payment and payment == payment_to_change:
+                pos_payment_ids = payment.ids + change_payment.ids
+                payment_amount = payment.amount + change_payment.amount
+            else:
+                pos_payment_ids = payment.ids
+                payment_amount = payment.amount
+
+            # UPDATE: Obtener recorsets de 'pos.payment':
+            recordsets_pos_payment_ids = self.env['account.move'].browse(pos_payment_ids)
+
+            payment_move = self.env['account.move'].with_context(default_journal_id=journal.id).create({
+                'journal_id': journal.id,
+                'date': fields.Date.context_today(order, order.date_order),
+                'ref': _('Pago de la factura para %(order)s (%(account_move)s) con %(payment_method)s', order=order.name, account_move=order.account_move.name, payment_method=payment_method.name),
+                'pos_payment_ids': pos_payment_ids,
+                # UPDATE: Sucursal:
+                'branch_id': recordsets_pos_payment_ids[0].branch_id.id if recordsets_pos_payment_ids and recordsets_pos_payment_ids[0].branch_id else False,
+            })
+            # raise UserError(f'payment_move: {payment_move.branch_id}')
+            result |= payment_move
+            payment.write({'account_move_id': payment_move.id})
+            amounts = pos_session._update_amounts({'amount': 0, 'amount_converted': 0}, {'amount': payment_amount}, payment.payment_date)
+            credit_line_vals = pos_session._credit_amounts({
+                'account_id': accounting_partner.with_company(order.company_id).property_account_receivable_id.id,  # The field being company dependant, we need to make sure the right value is received.
+                'partner_id': accounting_partner.id,
+                'move_id': payment_move.id,
+            }, amounts['amount'], amounts['amount_converted'])
+            is_split_transaction = payment.payment_method_id.split_transactions
+            if is_split_transaction and is_reverse:
+                reversed_move_receivable_account_id = accounting_partner.with_company(order.company_id).property_account_receivable_id.id
+            elif is_reverse:
+                reversed_move_receivable_account_id = payment.payment_method_id.receivable_account_id.id or self.company_id.account_default_pos_receivable_account_id.id
+            else:
+                reversed_move_receivable_account_id = self.company_id.account_default_pos_receivable_account_id.id
+            debit_line_vals = pos_session._debit_amounts({
+                'account_id': reversed_move_receivable_account_id,
+                'move_id': payment_move.id,
+                'partner_id': accounting_partner.id if is_split_transaction and is_reverse else False,
+            }, amounts['amount'], amounts['amount_converted'])
+            lines = self.env['account.move.line'].create([credit_line_vals, debit_line_vals])
+            if amounts['amount_converted'] < 0:
+                credit_line_ids += lines.filtered(lambda l: l.debit).ids
+            else:
+                credit_line_ids += lines.filtered(lambda l: l.credit).ids
+            payment_move._post()
+        return result.with_context(credit_line_ids=credit_line_ids)
+
+    # Se comentó porque genera error con las secuencias del Asiento del Pago
+    # Asientos contables del pago del punto de venta:
+    # def _create_payment_moves(self, is_reverse=False):
+    #     res = super(PosPayment, self)._create_payment_moves(is_reverse)
+    #     # Nota: res son registros de account.move
+    #     for rec in res:
+    #         if rec.pos_payment_ids:
+    #             payment_id = rec.pos_payment_ids[0]
+    #             if payment_id.branch_id:
+    #                 rec.write({
+    #                     'branch_id': payment_id.branch_id.id
+    #                 })
+    #     return res
 
 class PosPaymentMethod(models.Model):
     _inherit = "pos.payment.method"
